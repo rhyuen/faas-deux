4c99b20cb3a1de472d33eadbc4d73329b6e7fe30
Robert Yuen <dungeonry@hotmail.com>
2018-06-09 16:03:51 -0700

    netlify.toml and public site added.

--- .netlify/functions/firstfunction.js
+++
@@ -1,5 +0,0 @@
-exports.handler = (evt, ctx, cb) => {
-    //evt has path, method, headers, query, body
-    //ctx has iden info.
-    cb(null, {statusCode: 200, body: "Hi guys."});
-};
---
+++ functions/firstfunction.js
@@ -0,0 +1,11 @@
+exports.handler = (evt, ctx, cb) => {
+    //evt has path, method, headers, query, body
+    //ctx has iden info.
+    cb(null, {
+        statusCode: 200, 
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: "Hi guys."
+    });
+};
