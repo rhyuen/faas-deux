3807f3bab401ab919fde93fb9b056a7541184489
Robert Yuen <dungeonry@hotmail.com>
2018-08-09 03:49:26 -0700

    index page will call function six.js

---
+++ functions/six.js
@@ -0,0 +1,14 @@
+exports.handler = (evt, ctx, cb) => { 
+    const message = {
+        data: ["apple", "orange", "lemon", "lime", "blueberry", "grape", "plum"];
+    };
+
+    cb(null, {
+        statusCode: 200, 
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    });
+};
