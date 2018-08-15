856de9260f42883de9cadd0b641744f5a2e2c44d
Robert Yuen <dungeonry@hotmail.com>
2018-08-14 17:18:25 -0700

    testing ad with faas

--- functions/eight.js
+++ functions/eight.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 exports.handler = (evt, ctx, cb) => {
     //express not in the package and being declared in the file results in an error.
-    //express IN the package and declared...let's see how that works.
+    //express IN the package and declared...let's see how that works.  No, it doesn't work.
     //uuid works when in the package.json and node_modules added to version control.
     const app = express();
 
