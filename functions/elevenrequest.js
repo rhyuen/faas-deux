52a18690a4fb360e75757528790c839b8c8f06c5
Robert Yuen <dungeonry@hotmail.com>
2018-08-19 03:14:22 -0700

    request for intermediary.

---
+++ functions/elevenrequest.js
@@ -0,0 +1,18 @@
+const request = require("request");
+exports.handler = (evt, ctx, cb) => {
+
+    request("https://reddit.com/.json", (err, res, body) => {
+        if(err){
+            return cb(err);    
+        }
+        cb(null, {
+            statusCode: 200, 
+            headers: {
+                "Access-Control-Allow-Origin": "*",            
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        });
+    });   
+}
+
