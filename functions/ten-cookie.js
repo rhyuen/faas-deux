4207f9f884fe994aeb9d132ec863e928e7bffb59
Robert Yuen <dungeonry@hotmail.com>
2018-08-18 19:10:55 -0700

    cookie testing

---
+++ functions/ten-cookie.js
@@ -0,0 +1,26 @@
+
+exports.handler = (evt, ctx, cb) => {        
+    const list = [
+        "https://royuone.netlify.com/images/one.jpg",
+        "https://royuone.netlify.com/images/two.jpg",
+        "https://royuone.netlify.com/images/three.jpg",
+        "https://royuone.netlify.com/images/four.jpg",
+        "https://royuone.netlify.com/images/five.jpg",
+        "https://royuone.netlify.com/images/six.jpg",
+        "https://royuone.netlify.com/images/seven.jpg",
+        "https://royuone.netlify.com/images/eight.jpg",
+    ];
+
+    const response = {addresses: list};
+
+    cb(null, {
+        statusCode: 200, 
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+            "Set-Cookie": "newestcookie=testingcookie",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(response)
+    });
+};
