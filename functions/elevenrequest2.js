32ae794a3571be459d7892180a88910559ab5eef
Robert Yuen <dungeonry@hotmail.com>
2018-08-19 18:11:39 -0700

    Axios attempt with the ext library in scope.

--- functions/elevenrequest2.js
+++ functions/elevenrequest2.js
@@ -1,6 +1,7 @@
-const axios = require("axios");
-
 exports.handler = (evt, ctx, cb) => {
+    //If Ext library is out of scope of handler then it doesn't work.
+    //Odd how it works for uuid though.
+    const axios = require("axios");
     
     axios.get("https://reddit.com/.json")
         .then(res => {
