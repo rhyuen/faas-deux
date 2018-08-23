a61d589f1dfa01572a21738198db9f7f471a701d
Robert Yuen <dungeonry@hotmail.com>
2018-08-22 17:38:58 -0700

    Renamed some image files that were improperly spelled.  Added comments for fs function that attempts to read a file.

--- functions/twelvefs.js
+++ functions/twelvefs.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 exports.handler = (evt, ctx, cb) => {
     const fileUrl = "./sample.txt";
+    //Doesn't Work.  File not found. when the function starts.
     fs.readFile(fileUrl, (err, data) => {
         if (err){
             cb(err);
